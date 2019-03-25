016bcd9a56b134fd6e183f07c3afff917b34ec80
Dailen Hertog <hertogdailen@hotmail.nl>
2019-03-25 12:59:01 +0100

    Adds the feature to pickup the USB-Stick and apply it on the PC

--- WorkshopTeam2/Assets/Scripts/Task3PlayerController.cs
+++ WorkshopTeam2/Assets/Scripts/Task3PlayerController.cs
@@ -7,8 +7,12 @@ public class Task3PlayerController : MonoBehaviour
     public float moveSpeed = 7;
     public float turnSpeed = 100;
 
+    private GameObject usbStick = null;
+
     void Update()
     {
+        hitColliders = Physics.OverlapSphere(transform.position, 2, mask.value);
+
         //Move forward
         if (Input.GetKey(KeyCode.W))
         {
@@ -29,6 +33,10 @@ public class Task3PlayerController : MonoBehaviour
         {
             Turn(1);
         }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Interact();
+        }
     }
 
     //Move towards a direction: Positive is Forward, Negative is Backward
@@ -41,5 +49,35 @@ public class Task3PlayerController : MonoBehaviour
     private void Turn(int direction)
     {
         transform.Rotate(Vector3.up * direction * turnSpeed * Time.deltaTime);
+    }
+
+
+    private Vector3 lastPosition;
+    private Vector2 adjstGUI;
+    private LayerMask mask = -1;
+
+    private Collider[] hitColliders;
+
+    void Interact()
+    {
+        for (var i = 0; i < hitColliders.Length; i++)
+        {
+            if (hitColliders[i] != null)
+            {
+                if (hitColliders[i].gameObject.tag == "USB-Stick")
+                {
+                    USBStickController controller = hitColliders[i].gameObject.GetComponent<USBStickController>();
+                    controller.Interact();
+                    if (usbStick == null)
+                    {
+                        usbStick = hitColliders[i].gameObject;
+                    }
+                    else
+                    {
+                        usbStick = null;
+                    }
+                }
+            }
+        }
     }
 }
---
+++ WorkshopTeam2/Assets/Scripts/USBStickController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class USBStickController : MonoBehaviour
+{
+    public Transform player;
+    public bool pickedUp = false;
+
+
+    void LateUpdate()
+    {
+        if (pickedUp)
+        {
+            transform.position = player.position + new Vector3(0, 0, 0.5f);
+        }
+    }
+
+    public void Interact()
+    {
+        pickedUp = !pickedUp;
+    }
+
+    //Everything above can be removed when integrated with Player Controller!
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject otherObject = collision.gameObject;
+        if (otherObject.tag == "PC")
+        {
+            PCController controller = otherObject.GetComponent<PCController>();
+            controller.ApplyUSBStick();
+            Destroy(gameObject); //Remove this if u want to leave the USB-Stick in the scene! For example for animations or something.
+        }
+    }
+}
