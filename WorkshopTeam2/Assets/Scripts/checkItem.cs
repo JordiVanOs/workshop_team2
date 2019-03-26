6050b6a1af94783093707896a2e67c0bf3053a32
StudentPegi <47355193+StudentPegi@users.noreply.github.com>
2019-03-26 14:34:00 +0100

    Master version
    Latest working version.

--- WorkshopTeam2/Assets/Scripts/checkItem.cs
+++ WorkshopTeam2/Assets/Scripts/checkItem.cs
@@ -6,27 +6,25 @@ public class checkItem : MonoBehaviour
 {
     public AudioSource thx;
     public AudioSource bad;
+    bool missionComplete;
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "koffie")
+        if (collision.gameObject.tag == "koffie" && !missionComplete)
         {
            Debug.Log("Bedankt voor de heerlijke koffie");
             thx.Play();
+            missionComplete = true;
+
+            GameObject GO = GameObject.Find("Robot Main");
+            if (GO != null)
+                Destroy(GO.gameObject);
         }
-        if(collision.gameObject.tag != "koffie")
+        if(collision.gameObject.tag != "koffie" && !missionComplete)
         {
             Debug.Log("oprotten met die smerige rotzooi");
             bad.Play();
         }
     }
     
-
-     void Start()
-    {
-        
-    }
-    void Update()
-    {
-        
-    }
 }
